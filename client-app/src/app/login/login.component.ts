63cde4001c3ca143b6b87f7101cbeb3c9a66d8dd
yogaabdi80 <yogaabdipratama@gmail.com>
2020-12-14 00:36:24 +0700

    roll back ke setting localhost

--- client-app/src/app/login/login.component.ts
+++ client-app/src/app/login/login.component.ts
@@ -18,7 +18,9 @@ export class LoginComponent implements OnInit {
   private readonly client_id = "clientauthcode";
   private readonly response_type = "code";
   private readonly redirect_uri = "http://localhost:4200";
-
+  private readonly auth_uri = "http://localhost:8081"
+  public readonly auth_uri_google = `${this.auth_uri}/auth/server/oauth2/authorization/google`
+  public readonly auth_uri_facebook = `${this.auth_uri}/auth/server/oauth2/authorization/facebook`
 
   ngOnInit(): void {
   }
@@ -26,7 +28,7 @@ export class LoginComponent implements OnInit {
   login() {
     this.authService.login(this.username,this.password).subscribe(data => {
       if (data.status == 200) {
-        window.location.href=`http://localhost:8081/auth/server/oauth/authorize?client_id=${this.client_id}&response_type=${this.response_type}&redirect_uri=${this.redirect_uri}`
+        window.location.href=`${this.auth_uri}/auth/server/oauth/authorize?client_id=${this.client_id}&response_type=${this.response_type}&redirect_uri=${this.redirect_uri}`
       } else{
         alert(data.msg)
       }
